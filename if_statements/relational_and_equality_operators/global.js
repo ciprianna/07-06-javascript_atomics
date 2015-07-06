0bbbbe25c7ca9c037ea9944e006fd1b516053c42
Ciprianna <ciprianna17@gmail.com>
2015-07-06 15:53:46 -0500

    Finished comparing operators in 6A. Answered questions.

--- if_statements/relational_and_equality_operators/global.js
+++ if_statements/relational_and_equality_operators/global.js
@@ -1,17 +1,17 @@
 // Operators in the console log
-console.log( "5 == '5' is " + (5 == '5') ); //false
-console.log( "5 == 'five' is " + (5 == 'five') ); //false
-console.log( "5 == '6' is " + (5 == '6') ); //
-console.log( "5 === '5' is " + (5 === '5') );
-console.log( "5 == 2 + 3 is " + (5 == 2 + 3) );
-console.log( "5 == 5 is " + (5 == 5) );
-console.log( "5 === 5 is " + (5 === 5) );
-console.log( "true == 'true' is " + (true == 'true') );
-console.log( "true === 'true' is " + (true === 'true') );
-console.log( "true == false is " + (true == false) );
-console.log( "5 == 12 is " + (5 == 12) );
-console.log( "5 !== 12 is " + (5 != 12) );
-console.log( "5 < 12 is " + (5 < 12) );
-console.log( "5 <= 12 is " + (5 <= 12) );
-console.log( "5 > 12 is " + (5 > 12));
-console.log( "5 >= 12 is " + (5 >= 12));
+console.log( "5 == '5' is " + (5 == '5') ); // false
+console.log( "5 == 'five' is " + (5 == 'five') ); // false
+console.log( "5 == '6' is " + (5 == '6') ); // false
+console.log( "5 === '5' is " + (5 === '5') ); // false
+console.log( "5 == 2 + 3 is " + (5 == 2 + 3) ); // true
+console.log( "5 == 5 is " + (5 == 5) ); // true
+console.log( "5 === 5 is " + (5 === 5) ); // true
+console.log( "true == 'true' is " + (true == 'true') ); // false
+console.log( "true === 'true' is " + (true === 'true') ); // false
+console.log( "true == false is " + (true == false) ); // false
+console.log( "5 == 12 is " + (5 == 12) ); // false
+console.log( "5 !== 12 is " + (5 != 12) ); // true
+console.log( "5 < 12 is " + (5 < 12) ); // true
+console.log( "5 <= 12 is " + (5 <= 12) ); // true
+console.log( "5 > 12 is " + (5 > 12)); // false
+console.log( "5 >= 12 is " + (5 >= 12)); // false
