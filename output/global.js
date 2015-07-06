26e317b776cf4a38ec4892c5c77246a0453d2857
Ciprianna <ciprianna17@gmail.com>
2015-07-06 11:34:18 -0500

    Filled in .js file with the console.log() statements and alerts.
    Answered questions in the html file.

---
+++ output/global.js
@@ -0,0 +1,12 @@
+// Initial alert message when the user logs in;
+alert("Hi, I'm Gandolf");
+
+// Console log statements. Not shown to the user, but rest in a log file.
+console.log("My favorite breakfast is cinnamin toast with nutella.");
+console.log("I live with two cats.");
+console.log("My favorite place to eat lots of food is the Shire.");
+console.log("I take at least 5 naps per week.");
+console.log("I would take 10 naps per week if I could.");
+
+// Second alert, appears right after first alert to the user.
+alert("Goodbye!");
