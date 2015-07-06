96ce06614962f1d4cf6ac0e6805404b98f61ff9e
Ciprianna <ciprianna17@gmail.com>
2015-07-06 13:27:06 -0500

    Added the .js tasks for getting_input_with_prompt.

---
+++ getting_input_with_prompt/global.js
@@ -0,0 +1,17 @@
+// Using prompts to get information from the user
+var UserName = prompt("What's your name?", "Enter name");
+var UserAge = prompt("How old are you?", "Enter age");
+var UserHometown = prompt("What is your hometown?", "Enter hometown");
+var UserFavoriteNumber = prompt("What's your favorite number?", "Enter a number");
+
+// Output using alerts
+alert("Your name is " + UserName + ".");
+alert("You are " + UserAge + " years old.");
+alert("You're from " + UserHometown + ".");
+alert("Finally, your favorite number is " + UserFavoriteNumber + ".");
+
+// Output to the console.log()
+console.log("Your name is " + UserName + ".");
+console.log("You are " + UserAge + " years old.");
+console.log("You're from " + UserHometown + ".");
+console.log("Finally, your favorite number is " + UserFavoriteNumber + ".");
