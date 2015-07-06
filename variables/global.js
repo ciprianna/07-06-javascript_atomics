6fe2877d501b1d55a9a194f0d6aeb9048dec1495
Ciprianna <ciprianna17@gmail.com>
2015-07-06 12:16:50 -0500

    Added files for variables folder. JS filled in to program specs.

---
+++ variables/global.js
@@ -0,0 +1,19 @@
+// Variable assignment
+var name = "Calypso";
+var age = 7;
+var hairColor = "Orange";
+
+// Change variable values
+name = "Harry";
+age = "16";
+hairColor = "Gray";
+
+// // Alert statements
+// alert("My name is " + name + ".");
+// alert("My age is " + age + ".");
+// alert("My fur is " + hairColor + ".");
+
+// Using console.log() instead of alerts
+console.log("My name is " + name + ".");
+console.log("My age is " + age + ".");
+console.log("My fur is " + hairColor + ".");
