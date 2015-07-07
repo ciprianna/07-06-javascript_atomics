63b340a3089ebf34336ea2ff326752fb0663aeec
Ciprianna <ciprianna17@gmail.com>
2015-07-06 23:29:43 -0500

    Changed fizzy_drink to be a for loop.

--- loops/what_for/fizzy_drink/global.js
+++ loops/what_for/fizzy_drink/global.js
@@ -1,8 +1,5 @@
-var Bottles = 99;
-
-while (Bottles > 1) {
+for (Bottles = 99; Bottles > 1; Bottles--) {
   console.log(Bottles + " bottles of beer on the wall, " + Bottles + " bottles of beer.  You take one down, pass it around, " + (Bottles - 1) + " bottles of beer on the wall.");
-  Bottles--;
 }
 
 if (Bottles == 1) {
