7780d6904a6d6b416a9883d9e40afcb34be06625
Ciprianna <ciprianna17@gmail.com>
2015-07-06 22:34:24 -0500

    Added counting up or down with the user's incrementation.

--- loops/more_counting/global.js
+++ loops/more_counting/global.js
@@ -1,8 +1,10 @@
 var StartInput = prompt("What number should we start with?");
 var EndInput = prompt("Where should we end?");
+var IncrementInput = prompt("What increment would you like to count by?");
 
 var StartNumber = parseInt(StartInput);
 var EndNumber = parseInt(EndInput);
+var IncrementNumber = parseInt(IncrementInput);
 
 console.log("Start number: " + StartNumber);
 console.log("End number: " + EndNumber);
@@ -10,11 +12,11 @@ console.log("End number: " + EndNumber);
 if (StartNumber < EndNumber) {
   while (StartNumber <= EndNumber) {
     console.log(StartNumber);
-    StartNumber++;
+    StartNumber += IncrementNumber;
   }
 } else {
   while (StartNumber >= EndNumber) {
     console.log(StartNumber);
-    StartNumber--;
+    StartNumber -= IncrementNumber;
   }
 }
