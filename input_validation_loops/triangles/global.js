17892d14aae3a1e1e65750a85a34962336edde19
Ciprianna <ciprianna17@gmail.com>
2015-07-07 00:10:10 -0500

    Re-did triangles for part 8D.

---
+++ input_validation_loops/triangles/global.js
@@ -0,0 +1,62 @@
+// Get inputs from the user
+var InputOne = prompt("What's the length of the first leg in the triangle?", "");
+var InputTwo = prompt("And the second leg?", "");
+var InputThree = prompt("And the third?", "");
+
+// Convert to Integers
+var LegOne = parseInt(InputOne);
+var LegTwo = parseInt(InputTwo);
+var LegThree = parseInt(InputThree);
+
+// Do addition for the legs
+OneAndTwo = LegOne + LegTwo;
+OneAndThree = LegOne + LegThree;
+TwoAndThree = LegTwo + LegThree;
+
+// Set true or false values for length of legs
+var GreaterThanThree = OneAndTwo > LegThree;
+var GreaterThanTwo = OneAndThree > LegTwo;
+var GreaterThanOne = TwoAndThree > LegOne;
+
+// Output to the console.log()
+console.log(LegOne);
+console.log(LegTwo);
+console.log(LegThree);
+console.log(LegOne + " + " + LegTwo + "= " + OneAndTwo);
+console.log("The sum of Leg 1 and Leg 2 is greater than Leg 3: " + GreaterThanThree);
+console.log(LegOne + " + " + LegThree + "= " + OneAndThree);
+console.log("The sume of Leg 1 and Leg 3 is greater than Leg 2: " + GreaterThanTwo);
+console.log(LegTwo + " + " + LegThree + "= " + TwoAndThree);
+console.log("The sum of Leg 2 and Leg 3 is greater than Leg 1: " + GreaterThanOne);
+
+// Alerts
+while (GreaterThanOne == false || GreaterThanTwo == false || GreaterThanThree == false) {
+  alert("This is not a valid trianlge");
+  var InputOne = prompt("What's the length of the first leg in the triangle?", "");
+  var InputTwo = prompt("And the second leg?", "");
+  var InputThree = prompt("And the third?", "");
+
+  var LegOne = parseInt(InputOne);
+  var LegTwo = parseInt(InputTwo);
+  var LegThree = parseInt(InputThree);
+
+  OneAndTwo = LegOne + LegTwo;
+  OneAndThree = LegOne + LegThree;
+  TwoAndThree = LegTwo + LegThree;
+
+  var GreaterThanThree = OneAndTwo > LegThree;
+  var GreaterThanTwo = OneAndThree > LegTwo;
+  var GreaterThanOne = TwoAndThree > LegOne;
+
+  console.log(LegOne);
+  console.log(LegTwo);
+  console.log(LegThree);
+  console.log(LegOne + " + " + LegTwo + "= " + OneAndTwo);
+  console.log("The sum of Leg 1 and Leg 2 is greater than Leg 3: " + GreaterThanThree);
+  console.log(LegOne + " + " + LegThree + "= " + OneAndThree);
+  console.log("The sume of Leg 1 and Leg 3 is greater than Leg 2: " + GreaterThanTwo);
+  console.log(LegTwo + " + " + LegThree + "= " + TwoAndThree);
+  console.log("The sum of Leg 2 and Leg 3 is greater than Leg 1: " + GreaterThanOne);
+}
+
+alert("This is a valid triangle");
