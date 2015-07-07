0d3309bbff02db559caa52e03fa55af560230373
Ciprianna <ciprianna17@gmail.com>
2015-07-07 00:00:05 -0500

    Finished 8C

---
+++ input_validation_loops/range/global.js
@@ -0,0 +1,5 @@
+var NumberChoice = prompt("Choose a number between 1 and 8", "");
+
+for (NumberChoice; (NumberChoice >= 1 && NumberChoice <= 8); NumberChoice = prompt("Choose a number between 1 and 8", ""));
+
+alert("Goodbye!");
